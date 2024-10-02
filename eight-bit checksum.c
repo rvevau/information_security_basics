100da7c596c315ee6222a889b46f4894121a1152
evgeniy_kiselev <90621422+rvevau@users.noreply.github.com>
2024-10-02 17:57:33 +0300

    Update eight-bit checksum.c

--- eight-bit checksum.c
+++ eight-bit checksum.c
@@ -3,22 +3,14 @@
 int main()
 {
 	FILE* file = fopen("summ.txt", "r");
-
 	int C = 0, T = 0, M = (255 + 1);
 	char ch;
-	
 	while ((ch = fgetc(file)) != EOF) {
-		//printf("%c", ch);
 		T += ch;
-		//printf("%d", T);
-		
 	}
 	fclose(file);
-
 	C = T % M;
-
 	printf("%d", C);
-   
 	return (0);
 }
 
